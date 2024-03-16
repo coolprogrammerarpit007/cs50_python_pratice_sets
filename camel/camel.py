9c12974c38eb364fe1a3e5cb0bf820c62e4663e0
Arpit <arpit.mishra.out@gmail.com>
2024-03-16 21:48:15 +0530

    "Clean my code"

--- camel/camel.py
+++ camel/camel.py
@@ -2,10 +2,9 @@
 string = input('camelCase: ');
 newString = '';
 
-
 for letter in string:
   if letter.isupper():   # check if letter is in upper case or not
-    newString = newString + '_' + letter; # Add _ to the string
+    newString = newString + '_' + letter; # Add "_" to the string
   else:
     newString = newString + letter; #Else add characters as it ease.
 
