e40470703ecd779e12da4eb4d1dfffa6ba8a050a
Arpit <arpit.mishra.out@gmail.com>
2024-03-19 06:56:29 +0530

    Problem 4 of week2 loops of cs50 python

--- plates/plates.py
+++ plates/plates.py
@@ -1,4 +1,4 @@
-# Incomplete program
+import string
 
 def main():
   plate = input("Plate: ")
@@ -8,23 +8,93 @@ def main():
     print('Invalid')
 
 def is_valid(plate):
-  # Writing my code here
-  flag = True
-  length = len(plate)
-  if length <2 or length > 6:
-    print('It Prints1')
+  # To check if entered input is size required or not
+  is_valid_length = is_valid_size(plate)
+
+  if not(is_valid_length):
     return False
   else:
-    # Check if first two characters of plate are characters or not
-    letters = plate[0:2]
-    for letter in letters:
-      if not(letter.isalpha()): # will check is letter is character or not
-        flag = False  
-      return flag
-
-    # check if number is used in middle of the plate
-    if is_alpha_numeric(plate):
+    # Check if plate must start with two letters
+    start_with_letters = is_start_with_letters(plate)
+    if(not(start_with_letters)):
       return False
     else:
-      True
+      # check if word contains digits in middle of word
+      middle_digit = is_middle_digit(plate)
+      # print("middle_digit: ",middle_digit)
+      if not(not(middle_digit)):
+        return False
+      else:
+        # To check whether first number in word is 0 or not
+        first_number_zero = is_first_number_zero(plate)
+        if first_number_zero:
+          return False
+        else:
+          # To check whether word contains puncuation or not
+          is_punctuation = has_punctuation(plate)
+          if is_punctuation:
+            return False
+          else:
+            return True
+    
+  
+
+# function to determine if entered input is of valid size or not
+
+def is_valid_size(word):
+  length = len(word)
+  if length >= 2 and length <= 6:
+    return True
+  else:
+    return False
+
+
+# function to determine whether statrt with letters or not
+
+def is_start_with_letters(word):
+  letters = word[0:2]
+  for letter in letters:
+    if not(letter.isalpha()):
+      return False
+  return True
+
+
+# function to check whether digits are in middle of a word or not
+
+def is_middle_digit(word):
+  # print(word)
+  for i in range(len(word)-1):
+    # to check if a digit is in middle if it is then return false
+    
+    if word[i].isdigit() and word[i+1].isalpha():
+      # print('Middle digiit in word: ')
+      return True
+  return False
+
+# function to chech whether first number is digit or not in python
+
+def is_first_number_zero(word):
+  contains_digit = False
+  for char in word:
+      if char.isdigit():
+        contains_digit = True
+        break
+
+  if contains_digit:  
+    numbers = []
+    for i in word:
+      if i.isdigit():
+        numbers.append(int(i))
+    return True if numbers[0] == 0 else False
+  else:
+    return False
+
+# function to check whether a word contains punctuation or not
+
+def has_punctuation(word):
+  for char in word:
+    if char in string.punctuation:
+      return True
+  return False
+
 main()
